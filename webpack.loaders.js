9d4cce8e8d28d7ee4f7002ffa6a7f645b1e48466
porkbrain <git@porkbrain.com>
2023-05-11 23:02:08 +0200

    Setting up React repo based on template

---
+++ webpack.loaders.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+
+module.exports = [
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+    type: "asset",
+    generator: {
+      filename: "./fonts/[name].[contenthash:8][ext]",
+    },
+  },
+  {
+    test: /\.(png|jpe?g|gif|webp|ico|svg)(\?.*)?$/,
+    type: "asset",
+    generator: {
+      filename: "./images/[name].[contenthash:8][ext]",
+    },
+  },
+];
